e4f6f15aeea3d6936bc0365574fddc5b2d106482
JuicyPeachSwap <79715012+JuicyPeachSwap@users.noreply.github.com>
2021-03-08 13:51:37 +0100

    Update peachPriceContracts.ts

--- src/constants/peachPriceContracts.ts
+++ src/constants/peachPriceContracts.ts
@@ -1,7 +1,7 @@
-const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x9066e87Bac891409D690cfEfA41379b34af06391', //  TACO address - to be updated
-  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56', //  TACO address - to be updated
-  lpAddress: '0x3BB3B98c0FABE30166E6B7D62aEef393249871E4', //  TACO address - to be updated
+const priceContracts: { cakeAddress: string; busdAddress: string; lpAddress: string } = {
+  cakeAddress: '0x9469B619f991a63b59C333f5ffc86FD3011Ce044', //  testnet address - to be updated
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xDEa2006B9AB89b1a4fc3f82d09E230B74583fD3d', //  testnet address - to be updated
 }
 
 export default priceContracts
